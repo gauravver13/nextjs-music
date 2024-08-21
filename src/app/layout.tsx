f11e3881192b2a6368df85740fbe4398fb03bb76
Gaurav Prasad <100416827+gauravver13@users.noreply.github.com>
2024-08-21 13:20:41 +0530

    fix bugs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,10 +20,12 @@ export default function RootLayout(
     <html lang="en" className="dark">
       <body className={inter.className}>
         <div className="relative w-full flex items-center justify-center">
-            <Navbar/>
+            <Navbar />
         </div>
         {children}
       </body>
     </html>
   );
 }
+
+
